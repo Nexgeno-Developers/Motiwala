5fd9f8c961088782db133fa7edfa9ee2141712e5
Mahtab Alam <mehtab.makent@gmail.com>
2024-02-28 15:01:08 +0530

    delete unwanted files and start home page design

--- resources/views/frontend/partials/css.blade.php
+++ resources/views/frontend/partials/css.blade.php
@@ -1,17 +1,15 @@
-<script src="https://www.google.com/recaptcha/api.js" defer></script>
+<!-- <script src="https://www.google.com/recaptcha/api.js" defer></script> -->
 
-<!------------------- seedling ---------------->
+<!------------------- motiwala ---------------->
+
 <link rel="shortcut icon" href="{{ asset('/assets/frontend/images/favicon.png') }}">
-<!------------------- seedling ---------------->
 
+<!------------------- motiwala ---------------->
+<link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="/assets/frontend/css/style.css" />
 <link rel="stylesheet" href="/assets/frontend/css/responsive.css" />
-<link rel="stylesheet" href="/assets/frontend/css/aos.css" />
 <link rel="stylesheet" href="/assets/frontend/css/bootstrap.min.css" />
 <link rel="stylesheet" href="/assets/frontend/css/owl.carousel.min.css" />
 <link rel="stylesheet" href="/assets/frontend/css/owl.theme.default.min.css" />
-<link rel="stylesheet" href="/assets/frontend/css/whatsapp-chat-support.css" />
 <link rel="stylesheet" href="/assets/frontend/css/toastr.min.css" />
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
-
+<link rel= "stylesheet" href= "https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css" >
